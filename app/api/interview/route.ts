8b08336d505b258453e965e2a4b0643fc87a20e4
Dinesh <dineshdharavath03@gmail.com>
2026-07-01 21:43:05 +0530

    table creation

--- app/api/interview/route.ts
+++ app/api/interview/route.ts
@@ -1,11 +1,21 @@
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { MockInterview } from "@/utils/schema";
 
 export async function POST(req: Request) {
   const body = await req.json();
 
-  console.log(body);
+  const result = await db.insert(MockInterview).values({
+  jsonMockResp: body.jsonMockResp,
+  jobPosition: body.jobPosition,
+  jobDesc: body.jobDesc,
+  jobExperience: body.experience,
+  createdBy: "test@gmail.com",
+  createdAt: new Date().toISOString(),
+});
 
-  return NextResponse.json({
-    message: "Success",
-  });
+return NextResponse.json({
+  success: true,
+  result,
+});
 }
